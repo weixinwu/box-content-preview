652b4a07d4be926b99080f15709fdd4e11a7b179
Jakub Porebski <jporebski@box.com>
2022-06-29 00:42:54 +0200

    comment input init

--- src/lib/i18n.js
+++ src/lib/i18n.js
@@ -2,7 +2,7 @@
 let annotationMessages = {};
 
 try {
-    annotationMessages = require('box-annotations-messages').default; // eslint-disable-line
+    annotationMessages = require('box-annotations').default; // eslint-disable-line
 } catch (e) {} // eslint-disable-line
 
 const language = __LANGUAGE__ || 'en-US'; // eslint-disable-line
